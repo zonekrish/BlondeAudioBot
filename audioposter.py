2e2aaa2476fd892dec89a82d409024c2267f12d3
zonekrish <107716360+zonekrish@users.noreply.github.com>
2023-06-12 17:17:21 -0500

    Use mp3 over mp4

--- audioposter.py
+++ audioposter.py
@@ -1,5 +1,11 @@
 import ffmpeg
+import os
 
-audio = ffmpeg.input("selfcontrol.mp4", ss=43, t=3)
-audio = ffmpeg.output(audio, "clip.mp4")
+try:
+    os.remove("clip.mp3")
+except:
+    pass
+
+audio = ffmpeg.input("selfcontrol.mp3", ss=43, t=3)
+audio = ffmpeg.output(audio, "clip.mp3")
 ffmpeg.run(audio)
