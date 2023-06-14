5fd558a42a3a92227aa25f8e8f36f7f2df29a4fe
zonekrish <107716360+zonekrish@users.noreply.github.com>
2023-06-13 21:22:30 -0500

    Use ffprobe to get song length

--- audioposter.py
+++ audioposter.py
@@ -1,11 +1,32 @@
+# Install needed dependencies
 import ffmpeg
 import os
+import random
+import math
 
+# Remove clip if it's in the directory
 try:
     os.remove("clip.mp3")
 except:
     pass
 
-audio = ffmpeg.input("selfcontrol.mp3", ss=43, t=3)
+# Get random song from audio file + its duration
+song = "audio/" + random.choice(os.listdir("audio"))
+songLength = math.floor(float(ffmpeg.probe(song)["format"]["duration"]))
+
+# Get random time for clip to start
+while True:
+    time = random.randint(0, songLength-1)
+    
+    # Make sure clip doesn't start less than 3 seconds before end of song
+    if not(time + 3 > songLength):
+        break
+
+# Extract clip with ffmpeg
+audio = ffmpeg.input(song, ss=time, t=3)
 audio = ffmpeg.output(audio, "clip.mp3")
 ffmpeg.run(audio)
+
+# Add album cover to clip
+clip = ffmpeg.input("clip.mp3")
+cover = ffmpeg.input("cover.jpg")
