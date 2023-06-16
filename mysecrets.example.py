1adba9d4a30fe96493b7cff8621438f3e843cb74
zonekrish <107716360+zonekrish@users.noreply.github.com>
2023-06-16 16:05:03 -0500

    Update example file

--- mysecrets.example.py
+++ mysecrets.example.py
@@ -1,12 +1,15 @@
 # Here is where you'll put some information about your Twitter bot and other configuration stuff
 # Before touching this file, please rename this to "mysecrets.py"!!
+# This part is for configurating your program
+# For "cover_file", you want to add the file name for the image you want to use here (ex: "cover.jpg")
+# For "song_list", add the names of your songs you've added into the audio folder
 
-# This is part is for configurating your program
-# You want to add the file name for the image you want to use here (ex: "cover.jpg")
-# With more updates down the line, I'll add more stuff here
+# Ex: If I have 3 songs in the audio folder, mine would like this:
+# "song_list": sorted(["Nikes", "Ivy", "Pink + White"])
 
 config = {
-    "cover_file": "[insert image file name here]"
+    "cover_file": "[insert file name here]",
+    "song_list": sorted([])
 }
 
 # This is for the Twitter bot credentials
